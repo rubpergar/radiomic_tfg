f9eb66749fd7ce5fc88da6a4eafacc17def5a2c5
Ruben Perez <rupg29@gmail.com>
2025-04-08 17:30:16 +0200

    [feat]: add function to clear console

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,10 @@
+import platform
+import os
+
+
+#############################################################
+
+
 def yes_or_no(prompt):
     while True:
         response = input(prompt).strip().lower()
@@ -5,3 +12,16 @@ def yes_or_no(prompt):
             return response
         else:
             print("    [!] Please enter 'yes' or 'no'.")
+
+
+#############################################################
+
+
+def clear_console():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+#############################################################
